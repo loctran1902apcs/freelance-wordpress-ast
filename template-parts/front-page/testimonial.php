3f25807535b3bb156186fa0ceef23a75bbd35e30
Lộc Trần <mrloctran19@gmail.com>
2019-12-21 14:04:53 +0700

    Add Customer field for testimonial

--- template-parts/front-page/testimonial.php
+++ template-parts/front-page/testimonial.php
@@ -1,53 +1,35 @@
+<?php
+    $frontpage_id = get_option( 'page_on_front' );
+    $testimonial = get_field( "testimonial",$frontpage_id );
+    $customer_1 = get_field("customer_1",$frontpage_id);
+    $customer_2 = get_field("customer_2",$frontpage_id);
+    $customer_3 = get_field("customer_3",$frontpage_id);
+?>
+
 <div class="mt-5 front-certificate">
     <div class="container">
         <h1 class="pt-5 h1">Cảm nhận khách hàng</h1>
         <div class="row">
-            <div class="testimonial-wrapper">
-                <div class="testimonial-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</div>
-                <div class="testimonial-meta">
-                    <div class="testimonial-meta-inner">
-                        <div class="testimonial-image">
-                            <img src="https://i1.wp.com/vietcanvas.net/wp-content/uploads/2019/01/15355664_386703241662674_1678086824039252453_n.jpg?resize=300%2C300&ssl=1">
-                        </div>
-
-                        <div class="testimonial-details">
-                            <div class="testimonial-name">chị Ngọc Diễm</div>
-                            <div class="testimonial-job">Designer</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="testimonial-wrapper">
-                <div class="testimonial-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</div>
-                <div class="testimonial-meta">
-                    <div class="testimonial-meta-inner">
-                        <div class="testimonial-image">
-                            <img src="https://i0.wp.com/vietcanvas.net/wp-content/uploads/2019/01/17990591_10155166460348965_3834697697970594545_o.jpg?resize=300%2C300&ssl=1">
-                        </div>
-
-                        <div class="testimonial-details">
-                            <div class="testimonial-name">chị Đoan Nguyên</div>
-                            <div class="testimonial-job">Designer</div>
+            <?php
+                foreach($testimonial as $customer) {
+                    ?>
+                    <div class="testimonial-wrapper">
+                        <div class="testimonial-content"><?php echo $customer["comment"]; ?></div>
+                        <div class="testimonial-meta">
+                            <div class="testimonial-meta-inner">
+                                <div class="testimonial-image">
+                                    <img src="<?php echo $customer["image_url"];?>">
+                                </div>
+                                <div class="testimonial-details">
+                                    <div class="testimonial-name"><?php echo $customer["name"]; ?></div>
+                                    <div class="testimonial-job">Designer</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="testimonial-wrapper">
-                <div class="testimonial-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</div>
-                <div class="testimonial-meta">
-                    <div class="testimonial-meta-inner">
-                        <div class="testimonial-image">
-                            <img src="https://i1.wp.com/vietcanvas.net/wp-content/uploads/2019/01/35932373_2018389071506645_5210668204841500672_o.jpg?zoom=2&resize=150%2C150&ssl=1">
-                        </div>
-
-                        <div class="testimonial-details">
-                            <div class="testimonial-name">anh Nguyễn Văn Thành</div>
-                            <div class="testimonial-job">Designer</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <?php
+                }
+            ?>
         </div>
     </div>
 </div>
